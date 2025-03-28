0a6052ab58aaa8da1c0ec2cfd6e15dde12941db3
Geraltesus <ivanovgeorgij653@gmail.com>
2025-03-28 16:32:47 +0900

    add address ofr cors

--- myapp/cmd/app/main.go
+++ myapp/cmd/app/main.go
@@ -35,7 +35,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "http://193.32.178.251:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
